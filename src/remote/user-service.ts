f3fcd3e1839c4103fbf16630cbb920f299584df7
neftaly polanco <polanconeftaly@gmail.com>
2020-05-15 17:07:43 -0400

    added user service

---
+++ src/remote/user-service.ts
@@ -0,0 +1,28 @@
+import { User } from "../dtos/user";
+import { ERSClient } from "./ers-client";
+import { NewUser } from "../dtos/new-user";
+
+export async function register(newUser: NewUser) {
+    let response = await ERSClient.post('/users', newUser);
+    return await response.data;
+}
+
+export async function getUsers() {
+    return await ERSClient.get('/users');
+}
+
+export async function getUserById(id: number) {
+    return await ERSClient.get(`/users/${id}`);
+}
+
+export async function getUserByUniqueKey(key: string, value: string) {
+    return await ERSClient.get(`/users?${key}=${value}`);
+}
+
+export async function updateUser(updatedUser: User) {
+    return await ERSClient.put('/users', updatedUser);
+}
+
+export async function deleteUserById(id: number) {
+    return await ERSClient.delete(`/users/${id}`);
+}
