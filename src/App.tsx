034d9ffd9a1ac1ede412f626a561d83957c2b1b1
neftaly polanco <polanconeftaly@gmail.com>
2020-05-15 17:10:59 -0400

    login action

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+import HomeComponent from './components/home-component/HomeContainer';
+import LoginComponent from './components/login-component/LoginContainer';
+import NavbarComponent from './components/navbar-component/NavbarContainer';
+import RegisterComponent from './components/register-component/RegisterContainer';
+
+import { Provider } from 'react-redux';
+import { store } from './Store';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Router>
+          <AppBar color="primary" position="static">
+            <Toolbar>
+              <Typography variant="h5" color="inherit">
+                <NavbarComponent />
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            <Route path='/home' render={() => <HomeComponent />} />
+            <Route path='/login' render={() => <LoginComponent />} />
+            <Route path='/register' render={() => <RegisterComponent />} />
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
