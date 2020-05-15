623a0ad73d18aaaba84ec9808247363c5d4be08a
neftaly polanco <polanconeftaly@gmail.com>
2020-05-15 17:08:01 -0400

    added auth service

---
+++ src/remote/auth-service.ts
@@ -0,0 +1,11 @@
+import { User } from "../dtos/user";
+import { ERSClient } from "./ers-client";
+
+export async function authenticate(username: string, password: string): Promise<User> {
+    let response = await ERSClient.post('/auth', {username, password});
+    return await response.data;
+}
+
+export async function invalidateSession() {
+    return await ERSClient.get('/auth');
+}
